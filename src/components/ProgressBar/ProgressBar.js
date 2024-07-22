6a113b2c627b615b6ee2c162a17ad6e24d453f6e
Otto A. Laitinen <otto.laitinen@mindler.fi>
2024-07-22 17:30:47 +0300

    progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled, { css } from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <BarContainer
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
+      size={size}
+    >
+      <BarValueContainer>
+        <BarValue value={value} />
+      </BarValueContainer>
+      <VisuallyHidden>Progress: {value}%</VisuallyHidden>
+    </BarContainer>
+  );
 };
+
+const SIZES = {
+  small: css`
+    height: 8px;
+    border-radius: 4px;
+  `,
+  medium: css`
+    height: 12px;
+    border-radius: 4px;
+  `,
+  large: css`
+    height: 24px;
+    border-radius: 8px;
+    padding: 4px;
+  `,
+};
+
+const BarContainer = styled.div`
+  width: 100%;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+
+  ${({ size }) => SIZES[size]}
+`;
+
+const BarValueContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const BarValue = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+  width: ${({ value }) => value}%;
+`;
 
 export default ProgressBar;
